6f5cb915a52a47be458d6e7c265190903caf5137
Elvis20-23 <kiubyes.15@gmail.com>
2023-11-22 19:26:18 -0500

    base datos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-}) -> name('xInicio');
+Route::get('/', [PagesController::class, 'fnIndex']) -> name('xInicio');
 
-Route::get('/saludo',function () {
+Route::get('/galeria/{numero?}',[PagesController::class,'fnGaleria']) -> where ('numero','[0-9]+') -> name('xGaleria');
+
+Route::get('/lista',[PagesController::class,'fnLista']) -> name('xLista');
+
+/*Route::get('/', function () {
+    return view('welcome');
+}) -> name('xInicio');*/
+
+/*Route::get('/saludo',function () {
     return "Hola mundo desde laravel...";
-});
+});*/
 
 Route::get('/galeria/{numero}',function ($numero) {
     return "Este es el codigo de la foto: ".$numero;
