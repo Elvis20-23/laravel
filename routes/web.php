8a61fa125f74750c9298e49817a49c2690bc098d
Elvis20-23 <kiubyes.15@gmail.com>
2023-11-28 19:17:36 -0500

    lista

--- routes/web.php
+++ routes/web.php
@@ -4,49 +4,18 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+route::get('/',[PagesController::class, 'fnIndex'])-> name('xInicio');
+Route::get('/galeria/{numero?}', [PagesController::class, 'fnGaleria']) -> where('numero', '[0-9]+') -> name('xGaleria');
+Route::get('/lista', [PagesController::class, 'fnLista']) -> name('xLista');
 
-Route::get('/', [PagesController::class, 'fnIndex']) -> name('xInicio');
-
-Route::get('/galeria/{numero?}',[PagesController::class,'fnGaleria']) -> where ('numero','[0-9]+') -> name('xGaleria');
-
-Route::get('/lista',[PagesController::class,'fnLista']) -> name('xLista');
-
-/*Route::get('/', function () {
-    return view('welcome');
-}) -> name('xInicio');*/
-
-/*Route::get('/saludo',function () {
-    return "Hola mundo desde laravel...";
-});*/
-
-Route::get('/galeria/{numero}',function ($numero) {
-    return "Este es el codigo de la foto: ".$numero;
-}) -> where('numero','[0-9]+');
-
-Route::view('/galeria','/pagGaleria', ['valor' => 15]) -> name('xGaleria');
-
-Route::get('/dashboard', function() {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/lista', function() {
-    return view('pagLista');
-}) -> name('xLista');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
