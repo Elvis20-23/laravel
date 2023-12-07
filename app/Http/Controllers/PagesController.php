d09fc9748bb876af3528785dacc71e5dc4ea6f4c
Elvis20-23 <kiubyes.15@gmail.com>
2023-12-06 19:12:50 -0500

    registrar, actualizar y eliminar

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,37 +12,40 @@ class PagesController extends Controller
     }
 
     public function fnEstDetalle($id){
-        $xDetAlumnos = Estudiante::findOrFail($id);
+        $xDetAlumnos = Estudiantes::findOrFail($id);
         return view('Estudiante.pagDetalle',compact('xDetAlumnos'));
     }
+    public function fnRegistrar (Request $request) {
 
-    public function fnRegistrar (Request $request){
+        //return $request;
 
-        $request -> validate([
+        $request ->validate([
             'codEst'=>'required',
             'nomEst'=>'required',
             'apeEst'=>'required',
             'fnaEst'=>'required',
             'turMat'=>'required',
             'semMat'=>'required',
-            'estMat'=>'required',
+            'estMat'=>'required'
+
         ]);
 
         $nuevoEstudiante = new Estudiantes;
 
         $nuevoEstudiante->codEst = $request->codEst;
-        $nuevoEstudiante->nomEst = $request->codEst;
-        $nuevoEstudiante->apeEst = $request->codEst;
-        $nuevoEstudiante->fnaEst = $request->codEst;
-        $nuevoEstudiante->turMat = $request->codEst;
-        $nuevoEstudiante->semMat = $request->codEst;
-        $nuevoEstudiante->estMat = $request->codEst;
+        $nuevoEstudiante->nomEst = $request->nomEst;
+        $nuevoEstudiante->apeEst = $request->apeEst;
+        $nuevoEstudiante->fnaEst = $request->fnaEst;
+        $nuevoEstudiante->turMat = $request->turMat;
+        $nuevoEstudiante->semMat = $request->semMat;
+        $nuevoEstudiante->estMat = $request->estMat;
 
         $nuevoEstudiante->save();
 
-        return back()->with('msj','Se registro con éxito...');
-    }
+        return back()->with('msj','Se registro con exito...');
 
+
+    }
     public function fnLista(){
         $xAlumnos = Estudiantes::all();
         return view('pagLista', compact('xAlumnos'));
@@ -55,6 +58,34 @@ class PagesController extends Controller
         return view('pagGaleria', compact('valor', 'otro'));
     }
 
+    public function fnEstActualizar($id){
+        $xActAlumnos = Estudiantes::findOrFail($id);
+        return view('Estudiante.pagActualizar', compact('xActAlumnos'))->with('msj', 'Se actualizó con éxito...');
+    }
+    
+
+    public function fnUpdate(Request $request, $id){
+
+        $xUpdateAlumnos = Estudiantes::findOrFail($id);
+
+        $xUpdateAlumnos -> codEst = $request -> codEst;
+        $xUpdateAlumnos -> nomEst = $request -> nomEst;
+        $xUpdateAlumnos -> apeEst = $request -> apeEst;
+        $xUpdateAlumnos -> fnaEst = $request -> fnaEst;
+        $xUpdateAlumnos -> turMat = $request -> turMat;
+        $xUpdateAlumnos -> semMat = $request -> semMat;
+        $xUpdateAlumnos -> estMat = $request -> estMat;
+
+        $xUpdateAlumnos -> save();
+
+        return back()->with('msj','Se actualizo con éxito...');
+    }
+
+    public function fnEliminar($id){
+        $deleteAlumno = Estudiantes::findOrFail($id);
+        $deleteAlumno->delete();
+        return back()->with('msj','Se elimino con éxito ...');
+    }
 
     //
 }
